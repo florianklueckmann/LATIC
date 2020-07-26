9ba19a07673592e84eee00a5687e855b0359a515
florianklueckmann <3klueckm@informatik.uni-hamburg.de>
2020-07-26 10:48:26 +0200

    DeploymentAndExecution: Runs via IDE

--- src/main/java/dev/florianklueckmann/PrimaryController.java
+++ src/main/java/dev/florianklueckmann/PrimaryController.java
@@ -3,10 +3,20 @@ package dev.florianklueckmann;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
+import edu.stanford.nlp.io.IOUtils;
+
+import edu.stanford.nlp.simple.*;
+
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML
+    private void calculateStuff() throws IOException {
+        System.out.println("Hi");
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dev.florianklueckmann {
     requires javafx.controls;
     requires javafx.fxml;
+    requires stanford.corenlp;
+
 
     opens dev.florianklueckmann to javafx.fxml;
     exports dev.florianklueckmann;
