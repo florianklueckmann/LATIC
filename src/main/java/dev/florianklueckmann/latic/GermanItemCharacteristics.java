a2f0bcda143679b2b06dad3941d26f8c69b50a90
Florian Klückmann <3klueckm@informatik.uni-hamburg.de>
2021-02-13 18:57:41 +0100

    Move punctuation and similar characteristics from wordclass to wordlevel (#16)

--- src/main/java/dev/florianklueckmann/latic/GermanItemCharacteristics.java
+++ src/main/java/dev/florianklueckmann/latic/GermanItemCharacteristics.java
@@ -1,23 +1,9 @@
 package dev.florianklueckmann.latic;
 
 public enum GermanItemCharacteristics implements ItemCharacteristics {
-//    ADJECTIVES("Adjectives", "adjectives"),
     ADPOSITIONS( "adpositions", TaskLevel.WORD_CLASS),
-//    ADVERBS("Adverbs", "adverbs"),
     COORDINATING_CONJUNCTIONS("coordinatingConjunctions", TaskLevel.WORD_CLASS),
     SUBORDINATING_CONJUNCTIONS( "subordinatingConjunctions", TaskLevel.WORD_CLASS);
-//    DETERMINER("Determiner", "determiner"),
-//    PROPER_NOUNS("Proper nouns", "properNouns");
-//    INTERJECTIONS("Interjections", "interjections"),
-//    MODAL("Modals", "modals"),
-//    NOUNS("Nouns", "nouns"),
-//    NUMBERS("Numbers", "numbers"),
-//    PARTICLES("Particles", "particles"),
-//    PRONOUNS("Pronouns", "pronouns"),
-//    SYMBOLS("Symbols", "symbols"),
-//    VERBS("Verbs", "verbs"),
-//    PUNCTUATION("Punctuation", "punctuation"),
-//    UNKNOWN("Unknown", "unknown");
 
     private final TaskLevel level;
 
