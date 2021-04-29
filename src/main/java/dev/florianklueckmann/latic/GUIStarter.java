d4c0cf26d83a3b9ca2734391d6dc08f66f39ae6b
Florian Klueckmann <f_dev@mailbox.org>
2021-04-29 07:39:23 +0200

    Make code more readable

--- src/main/java/dev/florianklueckmann/latic/GUIStarter.java
+++ src/main/java/dev/florianklueckmann/latic/GUIStarter.java
@@ -1,5 +1,8 @@
 package dev.florianklueckmann.latic;
 
+/**
+ * This class is used as a workaround to start the application in an IDE
+ */
 public class GUIStarter {
 
     public static void main(final String[] args) {
--- src/main/java/dev/florianklueckmann/latic/services/CsvReader.java
+++ src/main/java/dev/florianklueckmann/latic/services/CsvReader.java
@@ -5,6 +5,7 @@ import dev.florianklueckmann.latic.App;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CsvReader {
@@ -17,7 +18,8 @@ public class CsvReader {
     public List<String> readFile(String fileName) {
         System.out.println(fileName);
         List<String> lines = new ArrayList<>();
-        try (Scanner scanner = new Scanner(new File(App.class.getResource(fileName).getPath()));) {
+        try (Scanner scanner = new Scanner(new File(Objects.requireNonNull(
+                App.class.getResource(fileName), "fileName must not be null").getPath()))) {
             while (scanner.hasNextLine()) {
                 lines.add(scanner.nextLine());
             }
