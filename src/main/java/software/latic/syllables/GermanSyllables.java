fe68fdd6104502bcd36d7c779bcf42f2fc3d3219
Florian Klueckmann <f_dev@mailbox.org>
2021-08-13 09:24:18 +0200

    Add special replacements to GermanSyllables

--- src/main/java/software/latic/syllables/GermanSyllables.java
+++ src/main/java/software/latic/syllables/GermanSyllables.java
@@ -10,16 +10,21 @@ public class GermanSyllables implements Syllables {
     }
 
     String vocals = "[aeiouyäöüéáà]";
-    String pseudoVocals = "ei|au|ie|eu|äu|aa|oo";
+    String pseudoVocals = "ei|au|ie|eu|äu|aa|oo|ee";
     String pseudoConsonants = "qu";
-    String specialCases = "ien$";
+    String specialOneSyllable = "eau|oire|sance";
+    String specialTwoSyllables = "ouille|tiell|ziell";
+    String bePrefixTwoSyllables = "(be)eng|end|eid|ehr|eil|erb|erd|ein";
 
     //TODO syllablesPerToken to adjust syllables based on wordClass
+    //TODO function for each replacment for better readability?
     public int syllablesPerWord(String word) {
         var syllableCount = 0;
 
         var codedChars = word.toLowerCase(Locale.ROOT)
-                .replaceAll("ien$", "001")
+                .replaceAll(bePrefixTwoSyllables, "00")
+                .replaceAll(specialTwoSyllables, "00")
+                .replaceAll(specialOneSyllable, "0")
                 .replaceAll("sch[bcdfghjklmnpqrstvwxyz]", "1")
                 .replaceAll(pseudoConsonants,"1")
                 .replaceAll(pseudoVocals,"0")
