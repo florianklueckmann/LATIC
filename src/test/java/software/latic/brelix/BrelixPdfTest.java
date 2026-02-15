a3f6d47758d1b10f51a62581bca94079852c6adf
Florian Klückmann <f_dev@mailbox.org>
2026-02-15 21:24:06 +0100

    chore(tests): comment out `PDFReaderTest` and `BrelixPdfTest`

--- src/test/java/software/latic/brelix/BrelixPdfTest.java
+++ src/test/java/software/latic/brelix/BrelixPdfTest.java
@@ -1,94 +1,94 @@
-package software.latic.brelix;
-
-import edu.stanford.nlp.io.IOUtils;
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.simple.Sentence;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import software.latic.helper.FileContent;
-import software.latic.helper.PDFReader;
-import software.latic.item.GermanTextItemData;
-import software.latic.translation.Translation;
-
-import java.io.IOException;
-import java.util.Locale;
-import java.util.Properties;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-public class BrelixPdfTest {
-
-    @ParameterizedTest
-    @ValueSource(strings = {
-            ".testfiles/Die kleine Eule Lili.pdf",
-            ".testfiles/No-Title-Die kleine Eule Lili-1.pdf",
-            ".testfiles/EuleLiliShortNoImg.pdf"
-    })
-    void testBrelixWithPdf(String pdfPath) throws IOException {
-        FileContent fileContent = PDFReader.getInstance().getContent(pdfPath);
-        
-        StringBuilder sb = new StringBuilder();
-        for (CharSequence cs : fileContent.getContent()) {
-            sb.append(cs);
-        }
-        String text = sb.toString();
-        
-        assertNotNull(text, "Text should not be null for " + pdfPath);
-        System.out.println("[DEBUG_LOG] Testing file: " + pdfPath);
-        System.out.println("[DEBUG_LOG] Extracted text length: " + text.length());
-        System.out.println("[DEBUG_LOG] Extracted font size (mm): " + fileContent.getFontSizeMm());
-        
-        Properties props = new Properties();
-        try {
-            props.load(IOUtils.readerFromString("StanfordCoreNLP-german.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Translation.getInstance().setLocale(Locale.GERMAN);
-        Document doc = new Document(props, text);
-        GermanTextItemData data = new GermanTextItemData(text);
-        
-        // Populate basic metrics required for Brelix
-        int wordCount = 0;
-        for (Sentence sent : doc.sentences()) {
-            for (String word : sent.words()) {
-                if (!word.matches("\\W+")) {
-                    wordCount++;
-                }
-            }
-        }
-        
-        data.setWordCount(wordCount);
-        data.setSentenceCount(doc.sentences().size());
-        if (data.getSentenceCount() > 0) {
-            data.setAverageSentenceLengthWords((double) wordCount / data.getSentenceCount());
-        }
-        data.setPagesCount(fileContent.getPages());
-        data.setFontSizeMm(fileContent.getFontSizeMm());
-        
-        // TTR (simplified for test)
-        long uniqueWords = doc.sentences().stream()
-                .flatMap(s -> s.words().stream())
-                .filter(w -> !w.matches("\\W+"))
-                .map(String::toLowerCase)
-                .distinct()
-                .count();
-        if (wordCount > 0) {
-            data.setTypeTokenRatio((double) uniqueWords / wordCount);
-        }
-
-        System.out.println("[DEBUG_LOG] Word count: " + data.getWordCount());
-        System.out.println("[DEBUG_LOG] Sentence count: " + data.getSentenceCount());
-        System.out.println("[DEBUG_LOG] Pages: " + data.getPagesCount());
-        System.out.println("[DEBUG_LOG] TTR: " + data.getTypeTokenRatio());
-
-        BrelixAnalyzer.getInstance().analyze(data, doc);
-
-        System.out.println("[DEBUG_LOG] BRELIX 0 Score: " + data.getBrelix0Score() + " (Level: " + data.getBrelix0Level() + ")");
-        System.out.println("[DEBUG_LOG] BRELIX 1 Score: " + data.getBrelix1Score() + " (Level: " + data.getBrelix1Level() + ")");
-        System.out.println("[DEBUG_LOG] BRELIX 2 Score: " + data.getBrelix2Score() + " (Level: " + data.getBrelix2Level() + ")");
-        System.out.println("[DEBUG_LOG] BRELIX 3 Score: " + data.getBrelix3Score() + " (Level: " + data.getBrelix3Level() + ")");
-        System.out.println("[DEBUG_LOG] BRELIX 4 Score: " + data.getBrelix4Score() + " (Level: " + data.getBrelix4Level() + ")");
-        System.out.println("[DEBUG_LOG] BRELIX 5 Score: " + data.getBrelix5Score() + " (Level: " + data.getBrelix5Level() + ")");
-    }
-}
+//package software.latic.brelix;
+//
+//import edu.stanford.nlp.io.IOUtils;
+//import edu.stanford.nlp.simple.Document;
+//import edu.stanford.nlp.simple.Sentence;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.ValueSource;
+//import software.latic.helper.FileContent;
+//import software.latic.helper.PDFReader;
+//import software.latic.item.GermanTextItemData;
+//import software.latic.translation.Translation;
+//
+//import java.io.IOException;
+//import java.util.Locale;
+//import java.util.Properties;
+//
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//
+//public class BrelixPdfTest {
+//
+//    @ParameterizedTest
+//    @ValueSource(strings = {
+//            ".testfiles/Die kleine Eule Lili.pdf",
+//            ".testfiles/No-Title-Die kleine Eule Lili-1.pdf",
+//            ".testfiles/EuleLiliShortNoImg.pdf"
+//    })
+//    void testBrelixWithPdf(String pdfPath) throws IOException {
+//        FileContent fileContent = PDFReader.getInstance().getContent(pdfPath);
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (CharSequence cs : fileContent.getContent()) {
+//            sb.append(cs);
+//        }
+//        String text = sb.toString();
+//
+//        assertNotNull(text, "Text should not be null for " + pdfPath);
+//        System.out.println("[DEBUG_LOG] Testing file: " + pdfPath);
+//        System.out.println("[DEBUG_LOG] Extracted text length: " + text.length());
+//        System.out.println("[DEBUG_LOG] Extracted font size (mm): " + fileContent.getFontSizeMm());
+//
+//        Properties props = new Properties();
+//        try {
+//            props.load(IOUtils.readerFromString("StanfordCoreNLP-german.properties"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Translation.getInstance().setLocale(Locale.GERMAN);
+//        Document doc = new Document(props, text);
+//        GermanTextItemData data = new GermanTextItemData(text);
+//
+//        // Populate basic metrics required for Brelix
+//        int wordCount = 0;
+//        for (Sentence sent : doc.sentences()) {
+//            for (String word : sent.words()) {
+//                if (!word.matches("\\W+")) {
+//                    wordCount++;
+//                }
+//            }
+//        }
+//
+//        data.setWordCount(wordCount);
+//        data.setSentenceCount(doc.sentences().size());
+//        if (data.getSentenceCount() > 0) {
+//            data.setAverageSentenceLengthWords((double) wordCount / data.getSentenceCount());
+//        }
+//        data.setPagesCount(fileContent.getPages());
+//        data.setFontSizeMm(fileContent.getFontSizeMm());
+//
+//        // TTR (simplified for test)
+//        long uniqueWords = doc.sentences().stream()
+//                .flatMap(s -> s.words().stream())
+//                .filter(w -> !w.matches("\\W+"))
+//                .map(String::toLowerCase)
+//                .distinct()
+//                .count();
+//        if (wordCount > 0) {
+//            data.setTypeTokenRatio((double) uniqueWords / wordCount);
+//        }
+//
+//        System.out.println("[DEBUG_LOG] Word count: " + data.getWordCount());
+//        System.out.println("[DEBUG_LOG] Sentence count: " + data.getSentenceCount());
+//        System.out.println("[DEBUG_LOG] Pages: " + data.getPagesCount());
+//        System.out.println("[DEBUG_LOG] TTR: " + data.getTypeTokenRatio());
+//
+//        BrelixAnalyzer.getInstance().analyze(data, doc);
+//
+//        System.out.println("[DEBUG_LOG] BRELIX 0 Score: " + data.getBrelix0Score() + " (Level: " + data.getBrelix0Level() + ")");
+//        System.out.println("[DEBUG_LOG] BRELIX 1 Score: " + data.getBrelix1Score() + " (Level: " + data.getBrelix1Level() + ")");
+//        System.out.println("[DEBUG_LOG] BRELIX 2 Score: " + data.getBrelix2Score() + " (Level: " + data.getBrelix2Level() + ")");
+//        System.out.println("[DEBUG_LOG] BRELIX 3 Score: " + data.getBrelix3Score() + " (Level: " + data.getBrelix3Level() + ")");
+//        System.out.println("[DEBUG_LOG] BRELIX 4 Score: " + data.getBrelix4Score() + " (Level: " + data.getBrelix4Level() + ")");
+//        System.out.println("[DEBUG_LOG] BRELIX 5 Score: " + data.getBrelix5Score() + " (Level: " + data.getBrelix5Level() + ")");
+//    }
+//}
--- src/test/java/software/latic/helper/PDFReaderTest.java
+++ src/test/java/software/latic/helper/PDFReaderTest.java
@@ -1,37 +1,37 @@
-package software.latic.helper;
-
-import org.junit.jupiter.api.Test;
-import java.io.IOException;
-import static org.junit.jupiter.api.Assertions.*;
-
-public class PDFReaderTest {
-
-    @Test
-    void testFontSizeExtraction() throws IOException {
-        String pdfPath = ".testfiles/EuleLiliShortNoImg.pdf";
-        FileContent content = PDFReader.getInstance().getContent(pdfPath);
-        
-        assertNotNull(content);
-        double fontSizeMm = content.getFontSizeMm();
-        
-        // EuleLiliShortNoImg.pdf is expected to have standard font size (~12pt)
-        // 12pt * 0.352778 = 4.233336 mm
-        assertEquals(4.23, fontSizeMm, 0.1, "Font size should be around 4.23mm (12pt)");
-        
-        System.out.println("[DEBUG_LOG] File: " + pdfPath + " Font size: " + fontSizeMm + " mm");
-    }
-
-    @Test
-    void testLargeFontSizeExtraction() throws IOException {
-        String pdfPath = ".testfiles/Die kleine Eule Lili.pdf";
-        FileContent content = PDFReader.getInstance().getContent(pdfPath);
-        
-        assertNotNull(content);
-        double fontSizeMm = content.getFontSizeMm();
-        
-        // This file was found to have ~8.98mm in our previous run
-        assertTrue(fontSizeMm > 6.0, "Font size should be larger than 6mm for this file");
-        
-        System.out.println("[DEBUG_LOG] File: " + pdfPath + " Font size: " + fontSizeMm + " mm");
-    }
-}
+//package software.latic.helper;
+//
+//import org.junit.jupiter.api.Test;
+//import java.io.IOException;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class PDFReaderTest {
+//
+//    @Test
+//    void testFontSizeExtraction() throws IOException {
+//        String pdfPath = ".testfiles/EuleLiliShortNoImg.pdf";
+//        FileContent content = PDFReader.getInstance().getContent(pdfPath);
+//
+//        assertNotNull(content);
+//        double fontSizeMm = content.getFontSizeMm();
+//
+//        // EuleLiliShortNoImg.pdf is expected to have standard font size (~12pt)
+//        // 12pt * 0.352778 = 4.233336 mm
+//        assertEquals(4.23, fontSizeMm, 0.1, "Font size should be around 4.23mm (12pt)");
+//
+//        System.out.println("[DEBUG_LOG] File: " + pdfPath + " Font size: " + fontSizeMm + " mm");
+//    }
+//
+//    @Test
+//    void testLargeFontSizeExtraction() throws IOException {
+//        String pdfPath = ".testfiles/Die kleine Eule Lili.pdf";
+//        FileContent content = PDFReader.getInstance().getContent(pdfPath);
+//
+//        assertNotNull(content);
+//        double fontSizeMm = content.getFontSizeMm();
+//
+//        // This file was found to have ~8.98mm in our previous run
+//        assertTrue(fontSizeMm > 6.0, "Font size should be larger than 6mm for this file");
+//
+//        System.out.println("[DEBUG_LOG] File: " + pdfPath + " Font size: " + fontSizeMm + " mm");
+//    }
+//}
